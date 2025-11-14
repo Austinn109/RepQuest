938a91fc56bd5c4707610911499a3048fe9c2515
hulondm <dylanmhulon@gmail.com>
2025-11-14 10:32:21 -0600

    Added WorkoutDetailScreen
    Added functionality to the WorkoutCards so that clicking them takes you to a WorkoutDetailScreen with a top navigation bar with back button.

---
+++ RepQuest/app/src/main/java/edu/apsu/repquest/screens/WorkoutDetailScreen.kt
@@ -0,0 +1,50 @@
+package edu.apsu.repquest.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import edu.apsu.repquest.dataclasses.Exercise
+import edu.apsu.repquest.dataclasses.Workout
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WorkoutDetailScreen(
+    workoutId: String,
+    onNavigateBack: () -> Unit
+) {
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Workout Details") },
+                navigationIcon = {
+                    IconButton(onClick = onNavigateBack) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back"
+                        )
+                    }
+                }
+            )
+        }
+    ) {
+        // Placeholder Screen
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = workoutId)
+        }
+    }
+}
--- RepQuest/app/src/main/java/edu/apsu/repquest/ui/theme/RepQuestApp.kt
+++ RepQuest/app/src/main/java/edu/apsu/repquest/ui/theme/RepQuestApp.kt
@@ -5,16 +5,19 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import edu.apsu.repquest.navigation.NavigationDestination
 import edu.apsu.repquest.screens.CreateWorkout
 import edu.apsu.repquest.screens.GoalsScreen
 import edu.apsu.repquest.screens.HistoryScreen
 import edu.apsu.repquest.screens.SettingsScreen
 import edu.apsu.repquest.screens.StatsScreen
+import edu.apsu.repquest.screens.WorkoutDetailScreen
 import edu.apsu.repquest.screens.WorkoutScreen
 
 @Composable
@@ -59,11 +62,27 @@ fun RepQuestApp() {
                 WorkoutScreen(
                     onCreateWorkoutClick = {
                         navController.navigate("createWorkout")
+                    },
+                    onWorkoutClick = { workoutId ->
+                        navController.navigate("workoutDetail/$workoutId")
                     }
                 )
             }
             composable("createWorkout") {
                 CreateWorkout()
+            }
+            composable(
+                route = "workoutDetail/{workoutId}",
+                arguments = listOf(navArgument("workoutId") { type = NavType.StringType})
+            ) {
+                backStackEntry ->
+                val workoutId = backStackEntry.arguments?.getString("workoutId") ?: ""
+                WorkoutDetailScreen(
+                    workoutId = workoutId,
+                    onNavigateBack = {
+                        navController.popBackStack()
+                    }
+                )
             }
             composable(NavigationDestination.History.route) {
                 HistoryScreen()
